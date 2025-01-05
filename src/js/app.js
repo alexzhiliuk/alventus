9c8350f44daf483d533809a8ebef7d40f9a89f31
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2025-01-05 15:52:37 +0300

    partners

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ import * as burger from './modules/burger.js'
 import * as search from './modules/search.js'
 import * as dropmenu from './modules/dropmenu.js'
 import * as production from './modules/production.js'
+import * as companiesSlider from './modules/companiesSlider.js'
 
 // flsFunctions.isWebp()
 
